64a934c7b5aec8c630b1d7c829ea670eb1172e57
David Bedok <bedok.david@nik.uni-obuda.hu>
2012-10-06 19:58:29 +0200

    CHG: runup examples

--- beginnings/runup.php
+++ beginnings/runup.php
@@ -1,6 +1,6 @@
 <?php
 	include("library/progtheorems.lib.php");
-	$dataA = array ( 2, 3, 5, 12 );
+	$dataA = array ( 3, 4, 5, 8, 12, 17, 23, 42 );
 	$dataB = array ( 1, 2, 3, 8, 9 );
 	print_r(runUpSortedSet($dataA, $dataB));	
 ?>
